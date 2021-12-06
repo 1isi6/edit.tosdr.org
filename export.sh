3491fc53817837e9660360af1f3234b7b36a21bb
1isi6 <94762502+1isi6@users.noreply.github.com>
2021-12-06 08:49:28 +0500

    Update export.sh
    rm latest.dump*
    heroku pg:backups:capture --app edit-tosdr-org
    heroku pg:backups:download --app edit-tosdr-org
    pg_restore --verbose --clean --no-acl --no-owner -h localhost -d phoenix_development latest.dump
    rails db:migrate
    rails runner db/export_points_to_old_db.rb

--- export.sh
+++ export.sh
@@ -4,5 +4,3 @@ heroku pg:backups:download --app edit-tosdr-org
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d phoenix_development latest.dump
 rails db:migrate
 rails runner db/export_points_to_old_db.rb
-git checkout -- old_db/points/
-git status
